873a22a839a6e5ed2c58a4d855a9c2428910b06a
mahi3crab <mahi3@hawaii.edu>
2022-02-06 23:34:35 -1000

    zipList

--- ziplist.js
+++ ziplist.js
@@ -1,10 +1,15 @@
-function zipListTheSimpleWay(list1, list2) {
-  let finalList = [];
-  for (let i = 0; i < list1; i++) {
-    finalList.push(list1[i]);
-    finalList.push(list2[i]);
+function zipList(list1, list2) {
+  const finalList = [];
+  for (let i = 0; i < list1.length; i++) {
+    finalList.push(list1[i], list2[i]);
   }
   return finalList;
+}
+
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
 }
 
 console.log(zipListTheSimpleWay(['a', 'b', 'c'], [1, 2, 3]));
